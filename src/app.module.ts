52d1f209b6fa45849b24f78592935d6abefb1125
reznicadrian <63201272+reznicadrian@users.noreply.github.com>
2022-05-22 18:56:00 +0300

    posts resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { ConfigModule } from './config/config.module';
 import { ConnectionsModule } from './connections/connections.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [ConfigModule, ConnectionsModule, AuthModule, UsersModule],
+  imports: [ConfigModule, ConnectionsModule, AuthModule, UsersModule, PostsModule],
 })
 export class AppModule {}
